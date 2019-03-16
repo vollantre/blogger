5fd0ece010eef37844d924e176d92cd4ed9c0461
vollantre <rinbinary@gmail.com>
2019-03-16 16:38:58 -0400

    finished blog comments feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,6 +7,10 @@ class ArticlesController < ApplicationController
 
     def show
         @articles = Article.find(params[:id])
+
+        @comment = Comment.new
+        @comment.article_id = @articles.id
+
     end
 
     def new
